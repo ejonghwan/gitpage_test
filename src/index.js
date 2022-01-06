ca78350e3b6844f4fe5d23be12704eb3d008db06
jonghwan <>
2022-01-06 10:45:31 +0900

    Revert "test"
    This reverts commit b55c1f8a329e21b6ac58d1f616d54e782bfbfed6.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import img from '../src/assets/images/aa.jpeg'
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    const img = document.querySelector('.img')
-    img.innerHTML `
-        <img src=${img} />
-    `
-})
